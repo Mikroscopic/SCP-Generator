8891198f7f391246ae5beff72c6d7fab5922c854
Mike <mikro236@gmail.com>
2019-09-06 18:43:28 -0400

    Fix port assignment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/newscp', (req, res) => {
 	res.status(200).send(scp);
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
