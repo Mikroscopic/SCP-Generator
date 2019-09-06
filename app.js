b58ca252a67864bc3289e357de1407f820e9aab9
Mike <mikro236@gmail.com>
2019-09-06 18:34:35 -0400

    Fix filepath references in app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, './')));
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, '../index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/newscp', (req, res) => {
